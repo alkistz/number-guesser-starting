059f5286c24edce2efe14e638716ddbfdf4cabe4
Alkiviadis Tzaras <alkiviadistzaras@yahoo.gr>
2021-01-19 00:31:43 +0100

    Creation of two first functions

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ let currentRoundNumber = 1;
 
 // Write your code below:
 
+function generateTarget() {
+    Math.floor(Math.random() * 10);
+};
+
+function compareGuesses(user_guess, computer_guess, random_number) {
+    let user_distance = Math.abs(user_guess - random_number)
+    let computer_distance = Math.abs(computer_guess - random_number)
+
+    return user_distance >= computer_distance
+};
